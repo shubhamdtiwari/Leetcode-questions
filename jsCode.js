ecaff30830834242db6669b997ba6d4afcd15934
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-25 20:06:57 +0530

    Update jsCode.js

--- jsCode.js
+++ jsCode.js
@@ -1,10 +1,10 @@
 'use strict';
 // Question 2620 :-  Counter
+
 var createCounter = function (n) {
-  let x = -1;
-  return function () {
-    x++;
-    return n + x;
+  return () => {
+    console.log(n);
+    return n++;
   };
 };
 
@@ -12,3 +12,16 @@ const counter = createCounter(10);
 counter(); // 10
 counter(); // 11
 counter(); // 12
+
+// var createCounter = function (n) {
+//   let x = -1;
+//   return function () {
+//     x++;
+//     return n + x;
+//   };
+// };
+
+// const counter = createCounter(10);
+// counter(); // 10
+// counter(); // 11
+// counter(); // 12
