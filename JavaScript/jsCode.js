15e95540b7c8d822bec92910b401975f57a91b43
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-25 21:15:08 +0530

    Create jsCode.js

---
+++ JavaScript/jsCode.js
@@ -0,0 +1,44 @@
+'use strict';
+/*
+// Question 2620 :-  Counter
+
+var createCounter = function (n) {
+  return () => {
+    console.log(n);
+    return n++;
+  };
+};
+
+const counter = createCounter(10);
+counter(); // 10
+counter(); // 11
+counter(); // 12
+
+
+///////////////////////////////////
+// Other way
+
+// var createCounter = function (n) {
+//   let x = -1;
+//   return function () {
+//     x++;
+//     return n + x;
+//   };
+// };
+
+// const counter = createCounter(10);
+// counter(); // 10
+// counter(); // 11
+// counter(); // 12
+*/
+
+// Question :- 2667 Create Hello World Function
+
+var createHelloWorld = () => {
+  return (...args) => {
+    console.log('Hello World');
+  };
+};
+
+const f = createHelloWorld();
+f(); // "Hello World"
