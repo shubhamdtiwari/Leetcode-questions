03d0f434d8c56a99c9c93e91ab18b01d77962dfa
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-17 14:47:51 +0530

    Question :- 2665

--- JavaScript/jsCode.js
+++ JavaScript/jsCode.js
@@ -69,19 +69,40 @@ var expect = function (val) {
 
 // modified version of question 2704 To Be Or Not To Be
 
-const expect = (val) => {
-  return {
-    toBe: (expected) => {
-      if (val === expected) return true;
-      else throw new Error('Not Equal');
-    },
-    notTobe: (unexpected) => {
-      if (val !== unexpected) return true;
-      else throw new Error('Equal');
-    },
-  };
-};
+// const expect = (val) => {
+//   return {
+//     toBe: (expected) => {
+//       if (val === expected) return true;
+//       else throw new Error('Not Equal');
+//     },
+//     notTobe: (unexpected) => {
+//       if (val !== unexpected) return true;
+//       else throw new Error('Equal');
+//     },
+//   };
+// };
 /**
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
  */
+
+var createCounter = function (init) {
+  let count = init;
+  return {
+    increment: () => {
+      return ++count;
+    },
+    decrement: () => {
+      return --count;
+    },
+    reset: () => {
+      count = init;
+      return count;
+    },
+  };
+};
+
+const counter = createCounter(5);
+console.log(counter.increment()); // 6
+console.log(counter.reset()); // 5
+console.log(counter.decrement()); // 4
