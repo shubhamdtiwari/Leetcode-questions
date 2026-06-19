1f256910a4a9f4a90b37a71d5abe77703c38ec16
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-20 00:33:49 +0530

    Number of Digits
    Another way of finding the number of digits in num using Maths.log10

--- JavaLeet/EvenNumberOfDigits.java
+++ JavaLeet/EvenNumberOfDigits.java
@@ -22,6 +22,14 @@ public class EvenNumberOfDigits {
         return numberOfDigits % 2 == 0;
     }
 
+    static int digits2(int num) {
+          if(num < 0) {
+            num = num * -1;
+        }
+
+        return (int) (Math.log10(num)) + 1;
+    }
+
     static int digits(int num) {
         if(num < 0) {
             num = num * -1;
