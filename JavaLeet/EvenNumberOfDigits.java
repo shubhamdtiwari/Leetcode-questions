64e4baca9da6e912c80b3321ce70de658425cfb3
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-20 00:31:16 +0530

    Create EvenNumberOfDigits.java

---
+++ JavaLeet/EvenNumberOfDigits.java
@@ -0,0 +1,43 @@
+package JavaLeet;
+
+public class EvenNumberOfDigits {
+
+    public static void main(String[] args) {
+        
+    }
+
+    static int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num : nums){
+            if(even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static boolean even(int num) {
+        int numberOfDigits = digits(num);
+
+        return numberOfDigits % 2 == 0;
+    }
+
+    static int digits(int num) {
+        if(num < 0) {
+            num = num * -1;
+        }
+
+        if(num == 0) {
+            return 1;
+        }
+
+        int count = 0;
+        while(num > 0) {
+            count++;
+            num /= 10;
+
+        }
+        return count;
+    }
+    
+}
