964a3972679e1ea4bb9f490f31099c8e1fc7204e
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-22 00:07:10 +0530

    Update RunningSum.java
    Other method

--- JavaLeet/RunningSum.java
+++ JavaLeet/RunningSum.java
@@ -3,19 +3,27 @@ package JavaLeet;
 public class RunningSum {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4};
-        int[] ans = new int[arr.length];
-        System.out.println(ans);
+        // int[] ans = new int[arr.length];
+        // System.out.println(ans);
+
+        System.out.println(arr);
         
     }
 
-    static int[] runSum(int[] arr, int[] ans) {
+    static int[] runSum(int[] arr) {
 
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = arr[i] + sum;
-            ans[i] = sum;
+        // int sum = 0;
+        // for (int i = 0; i < arr.length; i++) {
+        //     sum = arr[i] + sum;
+        //     ans[i] = sum;
+        // }
+        // return ans;
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i] + arr[i -1];
         }
-        return ans;
+        return arr;
+
 
     }
     
