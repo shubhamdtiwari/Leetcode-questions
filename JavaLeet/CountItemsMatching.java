d2ff6c17169527c41d8a645c22d684d45a65700d
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-26 14:33:26 +0530

    Update CountItemsMatching.java
    Other Method

--- JavaLeet/CountItemsMatching.java
+++ JavaLeet/CountItemsMatching.java
@@ -44,5 +44,24 @@ public class CountItemsMatching {
         return result;
         }
 
+        static int countMatches2(List<List<String>> items, String ruleKey, String ruleValue){
+            int result = 0;
+            int index = 0;
+
+            if(ruleKey.equals("color")){
+                index = 1;
+            }else if(ruleKey.equals("name")){
+                index = 2;
+            }
+
+           for (int i = 0; i < items.size(); i++) {
+             if(items.get(i).get(index).equals(ruleValue)){
+                result++;
+             }
+           }
+
+           return result;
+        }
+
     }
 
