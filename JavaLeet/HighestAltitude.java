78c9fc14d0e0b15a38cf264e6e0e2f399393c5ae
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-26 15:19:47 +0530

    Q :-  1732
    Find the Highest Altitude

---
+++ JavaLeet/HighestAltitude.java
@@ -0,0 +1,31 @@
+package JavaLeet;
+
+public class HighestAltitude {
+    public static void main(String[] args) {
+       int[] gain = {-5,1,15,0,7};
+
+       int ans = largestAltitude(gain);
+       
+        System.out.println(ans);
+      
+        
+    }
+    static int largestAltitude(int[] gain) {
+        int initAlt = 0;
+        int[] finalAlt = new int[gain.length + 1];
+        finalAlt[0] = initAlt;
+        for(int i = 1; i < finalAlt.length; i++){
+            finalAlt[i] = finalAlt[i - 1] + gain[i - 1];
+        }
+       
+        int max = finalAlt[0];
+
+        for(int h : finalAlt) {
+            if( h > max) {
+                max = h;
+            }
+        }
+        return max;
+    }
+}
+
