76cb461be6272e740ca272d048309e263a83739c
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-27 23:42:54 +0530

    Create CountOdd.java

---
+++ JavaLeet/CountOdd.java
@@ -0,0 +1,30 @@
+package JavaLeet;
+
+public class CountOdd {
+    public static void main(String[] args) {
+        int[][] indices = {
+                        {0,1},
+                        {1,1},
+        };
+        int m = 2;
+        int n = 3;
+
+
+        int[] ans = cell(indices);
+
+        for( int el : ans){
+            System.out.println(el + " ");
+        };
+    }
+
+    static int[] cell(int[][] arr) {
+
+        for(int[] cell : arr) {
+           return cell;
+            }
+
+          
+        }
+    
+}
+
