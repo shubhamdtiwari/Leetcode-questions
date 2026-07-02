dbab883c9fbe8db7e1491ee34c12739ab8441445
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-07-02 15:18:26 +0530

    other method

--- JavaLeet/Arrays/maxPop.java
+++ JavaLeet/Arrays/maxPop.java
@@ -31,7 +31,29 @@ public class maxPop {
         return maxPopYear;
     }
 
+    static int maximumPopulation2(int[][] logs) {
+        int freq[] = new int[2051];
 
-    
+        for(int log[] : logs) {
+            int birth = log[0];
+            int death = log[1];
 
+            freq[birth] += 1;
+            freq[death] -= 1;
+        }
+
+        int currPop = 0;
+        int maxPop = 0;
+        int minYear = 2050;
+
+        for(int year=1950; year<=2050; year++) {
+            currPop += freq[year];
+            if(currPop > maxPop) {
+                maxPop = currPop;
+                minYear = year;
+            }
+        }
+
+        return minYear;
+    }
 }
