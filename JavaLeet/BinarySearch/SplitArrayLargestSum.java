b447c4f674314d2667e753c4c76ac0ab43acf011
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-08-03 20:18:57 +0530

    Create SplitArrayLargestSum.java

---
+++ JavaLeet/BinarySearch/SplitArrayLargestSum.java
@@ -0,0 +1,11 @@
+public class SplitArrayLargestSum {
+    public static void main(String[] args) {
+        int[] nums = {7,2,5,10,8};
+        int k = 2;
+
+    }
+
+    static int MinlargestSum(int[] nums, int k){
+        
+    }
+}
