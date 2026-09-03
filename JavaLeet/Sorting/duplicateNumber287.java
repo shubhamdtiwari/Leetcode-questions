c17cba72304954893d135e7a1868664164774a18
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-04 05:06:46 +0530

    Q :- 287 Duplicate Number - Another Method

--- JavaLeet/Sorting/duplicateNumber287.java
+++ JavaLeet/Sorting/duplicateNumber287.java
@@ -8,28 +8,42 @@ public class duplicateNumber287 {
     }
 
     static int findDuplicate(int[] nums) {
-        int ans = 0;
-        cycleSort(nums);
+        // int ans = 0;
+        // cycleSort(nums);
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i + 1) {
-                ans = nums[i];
-            }
-        }
-        return ans;
-    }
-
-    static void cycleSort(int[] arr) {
+        // for (int i = 0; i < nums.length; i++) {
+        // if (nums[i] != i + 1) {
+        // ans = nums[i];
+        // }
+        // }
+        // return ans;
 
         int i = 0;
-        while (i < arr.length) {
-            if (arr[i] != arr[arr[i] - 1]) {
-                swap(arr, i, arr[i] - 1);
+        while (i < nums.length) {
+            if (nums[i] != i + 1) {
+                if (nums[i] != nums[nums[i] - 1]) {
+                    swap(nums, i, nums[i] - 1);
+                } else {
+                    return nums[i];
+                }
             } else {
                 i++;
             }
         }
+        return -1;
     }
+
+    // static void cycleSort(int[] arr) {
+
+    // int i = 0;
+    // while (i < arr.length) {
+    // if (arr[i] != arr[arr[i] - 1]) {
+    // swap(arr, i, arr[i] - 1);
+    // } else {
+    // i++;
+    // }
+    // }
+    // }
 
     static void swap(int[] nums, int first, int second) {
         int temp = nums[first];
