be5b3f8bc923ca682bef798e04a408eeba632783
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-04 09:35:39 +0530

    Q :- 442 All Duplicates Numbers

---
+++ JavaLeet/Sorting/allDuplicates442.java
@@ -0,0 +1,41 @@
+package JavaLeet.Sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class allDuplicates442 {
+    public static void main(String[] args) {
+        int[] nums = { 4, 3, 2, 7, 8, 2, 3, 1 };
+        System.out.println(findDuplicates(nums));
+
+    }
+
+    static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                ans.add(nums[i]);
+            }
+        }
+
+        return ans;
+    }
+
+    static void sort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] != arr[arr[i] - 1]) {
+                swap(arr, i, arr[i] - 1);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
