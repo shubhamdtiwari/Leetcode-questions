254d657b361af4d719d2a89f1a73f348874d2f6e
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-05 12:30:14 +0530

    Q :- 645 Set mismatch

---
+++ JavaLeet/Sorting/setMismatch.java
@@ -0,0 +1,43 @@
+package JavaLeet.Sorting;
+
+public class setMismatch {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 2, 4 };
+        int[] finalAns = Mismatch(nums);
+        for (int el : finalAns) {
+            System.out.println(el);
+        }
+
+    }
+
+    static int[] Mismatch(int[] arr) {
+        int[] ans = { arr.length, arr.length };
+
+        sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1) {
+                ans[0] = arr[i];
+                ans[1] = i + 1;
+            }
+        }
+        return ans;
+    }
+
+    static void sort(int[] arr) {
+        int i = 0;
+
+        while (i < arr.length) {
+            if (arr[i] != arr[arr[i] - 1]) {
+                swap(arr, i, arr[i] - 1);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
