d453120dbba1e698e83c5a57e429eeef54e2f676
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-19 15:57:56 +0530

    Q :- 367 Valid Perfect Square

---
+++ JavaLeet/Searching/BinarySearch/validPerfectSquare367.java
@@ -0,0 +1,26 @@
+package JavaLeet.Searching.BinarySearch;
+
+public class validPerfectSquare367 {
+    public static void main(String[] args) {
+        System.out.println(perfectsqrt(6241));
+
+    }
+
+    static boolean perfectsqrt(int n) {
+        int s = 1;
+        int e = n;
+
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+
+            if (m == n / m && n % m == 0) {
+                return true;
+            } else if (m > n / m) {
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        return false;
+    }
+}
