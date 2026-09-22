907d198d6758aa5b8372a28d9e8eb31aeb4418ed
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-23 00:36:37 +0530

    Q :- 1539 kth missing positive number

---
+++ JavaLeet/Searching/BinarySearch/kthMIssingNumber.java
@@ -0,0 +1,25 @@
+package JavaLeet.Searching.BinarySearch;
+
+public class kthMIssingNumber {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 5, 6, 7 };
+        System.out.println(missingNumber(arr, 1));
+
+    }
+
+    static int missingNumber(int[] arr, int k) {
+        int s = 0;
+        int e = arr.length - 1;
+
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+
+            if (arr[m] - m - 1 < k) {
+                s = m + 1;
+            } else {
+                e = m - 1;
+            }
+        }
+        return s + k;
+    }
+}
