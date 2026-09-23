165ac9917aa82ac04e92537308d17b6e1e7c9c54
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-23 20:10:41 +0530

    Q :- 35 Search Insert Position

---
+++ JavaLeet/Searching/BinarySearch/searchInsertPosition35.java
@@ -0,0 +1,26 @@
+package JavaLeet.Searching.BinarySearch;
+
+public class searchInsertPosition35 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 5, 6, 7 };
+        System.out.println(InsertPosition(arr, 8));
+    }
+
+    static int InsertPosition(int nums[], int target) {
+        int s = 0;
+        int e = nums.length - 1;
+
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+
+            if (nums[m] < target) {
+                s = m + 1;
+            } else if (nums[m] > target) {
+                e = m - 1;
+            } else {
+                return m;
+            }
+        }
+        return s;
+    }
+}
