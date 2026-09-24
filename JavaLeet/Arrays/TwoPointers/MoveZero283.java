8756c14d635794958bc047be3137ad31c89aa4be
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-24 12:54:25 +0530

    Q :- 283 Move Zeros

---
+++ JavaLeet/Arrays/TwoPointers/MoveZero283.java
@@ -0,0 +1,29 @@
+package JavaLeet.Arrays.TwoPointers;
+
+import java.util.Arrays;
+
+public class MoveZero283 {
+    public static void main(String[] args) {
+        int[] arr = { 0, 2, 0, 3, 4, 6, 7 };
+        System.out.println(Arrays.toString(moveZero(arr)));
+
+    }
+
+    static int[] moveZero(int[] arr) {
+        int s = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                swap(arr, i, s);
+                s++;
+            }
+        }
+        return arr;
+    }
+
+    static void swap(int[] arr, int f, int se) {
+        int temp = arr[f];
+        arr[f] = arr[se];
+        arr[se] = temp;
+    }
+}
